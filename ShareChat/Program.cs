e8f85f589627a35641a88e0cf99ea636ab28c3b3
Shikha Singh <siya@Shikhas-MacBook-Pro.local>
2025-07-07 17:41:06 +0530

    adds cors to service

--- ShareChat/Program.cs
+++ ShareChat/Program.cs
@@ -11,9 +11,20 @@ builder.Services.AddDbContext<ChatDbContext>(options =>
 builder.Services.AddControllers();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<UserService>();
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy("AllowFrontend", policy =>
+  {
+    policy
+      .WithOrigins("http://localhost:5173")
+      .AllowAnyHeader()
+      .AllowAnyMethod();
+  });
+});
 
 var app = builder.Build();
 
+app.UseCors("AllowFrontend");
 app.MapControllers();
 
 app.Run();
